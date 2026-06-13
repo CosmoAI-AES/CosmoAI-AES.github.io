48af909fba83992852839fa6e6a475814972ba5e
Hans Georg Schaathun <georg@schaathun.net>
2026-06-13 12:56:17 +0200

    Try executing notebooks in workflows.

---
+++ README.m
@@ -0,0 +1,14 @@
+
+
+# Subtrees
+
+```
+git remote add testrepo git@github.com:CosmoAI-AES/cosmosim-test
+git subtree add --prefix demo testrepo main
+```
+
+```
+git subtree pull --prefix demo testrepo main
+git subtree push --prefix=demo testrepo main
+
+```
